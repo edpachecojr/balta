216cab6ba7f1858b4ea42084fd28adebee179fc3
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2021-03-14 18:06:04 -0300

    [refactor] criando entidade base

---
+++ ContentContext/Base.cs
@@ -0,0 +1,15 @@
+using System;
+using Balta.NotificationContext;
+
+namespace Balta.ContentContext
+{
+    public abstract class Base : Notifiable
+    {
+        public Guid Id { get; set; }
+
+        public Base()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Balta.ContentContext;
 
 namespace Balta
@@ -18,6 +19,29 @@ namespace Balta
                 Console.WriteLine(article.Id);
                 Console.WriteLine(article.Title);
                 Console.WriteLine(article.Url);
+            }
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseOOP);
+
+            var careers = new List<Career>();
+            var career = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+            career.Items.Add(careerItem);
+            careers.Add(career);
+
+            foreach (var item in careers)
+            {
+                Console.WriteLine(item.Title);
+                foreach (var cItem in item.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{cItem.Order} - {cItem.Title}");
+                }
             }
         }
     }
